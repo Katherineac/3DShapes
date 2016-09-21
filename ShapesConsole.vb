2bd403c3261dfbdc932de6f4e7272912e129b0f4
Austin <i.hazy.i@gmail.com>
2016-09-21 00:35:05 -0500

    Added ShapesConsole class with empty methods

---
+++ ShapesConsole.vb
@@ -0,0 +1,31 @@
+﻿Public Class ShapesConsole
+
+    ' Visual Studio won't let me name as shapes
+    Private shapeList As List(Of Shape)
+
+    Private Property Shapes As List(Of Shape)
+        Get
+            Return shapeList
+        End Get
+        Set(value As List(Of Shape))
+            shapeList = value
+        End Set
+    End Property
+
+    Public Sub CompositionRoot()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub Main()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub RenderShapes()
+        Throw New NotImplementedException()
+    End Sub
+
+
+
+
+
+End Class
