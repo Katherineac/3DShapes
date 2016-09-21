b7db40ca06d1e5540b408fc88480bba8335142cf
Austin <i.hazy.i@gmail.com>
2016-09-21 00:23:06 -0500

    Initial commit of Cone class, added empty Shape class

---
+++ Cone.vb
@@ -0,0 +1,26 @@
+﻿Public Class Cone
+    Inherits Shape
+
+    Property Height As Single
+    Property Radius As Single
+
+
+    Sub New()
+
+
+    End Sub
+
+    Public Function Render() As String
+        Return "Nothing"
+    End Function
+
+    Public Function SurfaceArea() As Single
+        Return System.Math.PI * Radius * (Radius + System.Math.Sqrt(System.Math.Pow(Height, 2) + System.Math.Pow(Radius, 2)))
+    End Function
+
+
+    Public Function Volume()
+        Return System.Math.PI * System.Math.Pow(Radius, 2) * (Height / 3)
+    End Function
+
+End Class
---
+++ Shape.vb
@@ -0,0 +1,2 @@
+﻿Public Class Shape
+End Class
