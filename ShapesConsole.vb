540ff007d3ae72d00f2bffb5ab7d80dc97a62d70
Austin Prueher <i.hazy.i@gmail.com>
2016-09-22 08:58:36 -0500

    added code to ShapesConsole

--- ShapesConsole.vb
+++ ShapesConsole.vb
@@ -1,6 +1,6 @@
 ﻿Public Class ShapesConsole
 
-    ' Visual Studio won't let me name as shapes
+    ' Switch to Irenderer
     Private shapeList As List(Of Shape)
 
     Private Property Shapes As List(Of Shape)
@@ -13,15 +13,21 @@
     End Property
 
     Public Sub CompositionRoot()
-        Throw New NotImplementedException()
+        shapeList = New List(Of Shape)
+        Shapes = New List(Of Shape)
+
     End Sub
 
     Public Sub Main()
-        Throw New NotImplementedException()
+        CompositionRoot()
+        RenderShapes()
     End Sub
 
     Public Sub RenderShapes()
-        Throw New NotImplementedException()
+        For Each currentShape As Shape In shapeList
+            currentShape.MessageBox.Show()
+        Next
+
     End Sub
 
 
