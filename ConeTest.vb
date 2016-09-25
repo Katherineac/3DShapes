a888d43173a849dae847988bbc07a14297b31494
Katherine Cater <kcater@student.cvtc.edu>
2016-09-24 19:15:17 -0500

    added test file

---
+++ ConeTest.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+
+Imports System
+Imports NUnit.Framework
+
+Namespace ConeTest
+
+    <TestFixture>
+    Public Class ConeTest
+        <Test>
+        Public Sub TestMethod()
+            'set cone height and radius
+            'call ShapesConsole.Main()?
+            'if surface area is correct then 
+            Assert.Pass("Your first passing test")
+            'else Assert.Fail("Your test failed")
+        End Sub
+
+    End Class
+
+End Namespace
--- ShapesConsole.vb
+++ ShapesConsole.vb
@@ -1,31 +1,51 @@
 ﻿Public Class ShapesConsole
 
-    ' Visual Studio won't let me name as shapes
-    Private shapeList As List(Of Shape)
+    ' Switch to Irenderer
+    Private shapeList As List(Of IRenderer)
+    Dim height = 5
+    Dim length = 5
+    Dim width = 5
+    Dim radius = 5
+    Dim diameter = 5
 
-    Private Property Shapes As List(Of Shape)
+    Private Property Shapes As List(Of IRenderer)
         Get
             Return shapeList
         End Get
-        Set(value As List(Of Shape))
+        Set(value As List(Of IRenderer))
             shapeList = value
         End Set
     End Property
 
     Public Sub CompositionRoot()
-        Throw New NotImplementedException()
+        shapeList = New List(Of IRenderer)
+        Shapes = New List(Of IRenderer)
+
+        Dim newCone As New Cone(height, radius)
+        Shapes.Add(newCone)
+
+        Dim newCylinder As New Cylinder(diameter, height, radius)
+        Shapes.Add(newCylinder)
+
+        Dim newSphere As New Sphere(diameter, radius)
+        Shapes.Add(newSphere)
+
+        Dim newCuboid As New Cuboid(height, length, width)
+        Shapes.Add(newCuboid)
+
     End Sub
 
-    Public Sub Main()
-        Throw New NotImplementedException()
+    Public Shared Sub Main()
+        Dim instance As New ShapesConsole()
+        instance.CompositionRoot()
+        instance.RenderShapes()
     End Sub
 
     Public Sub RenderShapes()
-        Throw New NotImplementedException()
+        For Each render As IRenderer In Shapes
+            render.Render()
+        Next
     End Sub
-
-
-
 
 
 End Class
